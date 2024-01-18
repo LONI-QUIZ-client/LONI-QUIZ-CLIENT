916b06617839d48e2faee1c38824eb0597974657
YoungHeeSo <apdlfdldjqtm@gmail.com>
2024-01-18 17:50:05 +0900

    로그인 버튼 생성

--- src/user/SignInTemplate.js
+++ src/user/SignInTemplate.js
@@ -5,6 +5,8 @@ import '../scss/sign-in.scss'
 import { BsPerson } from "react-icons/bs";
 import { BsLock } from "react-icons/bs";
 
+import kakaoSymbol from "../scss/img/kakao_symbol.png";
+
 const SignInTemplate = () => {
     return (
         <div className={"sign-in-full-screen"}>
@@ -15,12 +17,10 @@ const SignInTemplate = () => {
                     <div className={"sign-in-items"}>
                         <div className={"sign-in-id-item"}>
                             <BsPerson />
-                            {/*<div className={"sign-in-id-icon-cell"}></div>*/}
                             <input type={"text"} className={"sign-in-id-input"}/>
                         </div>
                         <div className={"sign-in-password-item"}>
                             <BsLock />
-                            {/*<div className={"sign-in-password-icon-cell"}></div>*/}
                             <input type={"password"} className={"sign-in-password-input"}/>
                         </div>
                         <div className={"auto-sign-in-check"}>
@@ -29,8 +29,13 @@ const SignInTemplate = () => {
                     </div>
                     <div className={"sign-in-buttons"}>
                         <div className={"sign-in-error-message"}></div>
-                        <div className={"loni-quiz-sign-in-button"}></div>
-                        <div className={"kakao-sign-in-button"}></div>
+                        <div className={"loni-quiz-sign-in-button"}>
+                            LONI-QUIZ 로그인
+                        </div>
+                        <div className={"kakao-sign-in-button"}>
+                            <img src={kakaoSymbol} alt={"카카오톡-심볼"}/>
+                            카카오 로그인
+                        </div>
                     </div>
                 </div>
             </div>
