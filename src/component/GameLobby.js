a0d4384eea3fc43aba3124405e0db28c0f878378
conan <sarimiro@naver.com>
2024-01-19 17:39:09 +0900

    로비 커밋 - 뼈대 구현 완료
    테마 변경 필요 + 기능 삽입 필요 + 창 사이즈 마다 객체 사이즈 설정 필요

--- src/component/GameLobby.js
+++ src/component/GameLobby.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './scss/GameLobby.scss';
+import './css/GameLobby.css';
 
 const GameLobby = () => {
     return (
@@ -13,41 +14,106 @@ const GameLobby = () => {
                             <p>0승 0패</p>
                         </div>
                         <div className='btns_box'>
-                            <button className='setting_btn'>환경설정</button>
-                            <button className='create_room_btn'>방만들기</button>
-                            <button className='start_game_btn'>게임시작</button>
+                            <button className='setting_btn'>Setting</button>
+                            <button className='create_room_btn'>Create Room</button>
+                            <button className='start_game_btn'>Game Start</button>
                         </div>
                     </div>
                 </div>
+
                 <div className= 'lobby_box2'>
                     <div className= 'lobby_bbox2'>
                         <div className='room_list'>
-                            <ul>
-                                <li>
-                                    <h2>방 번호: </h2>
-                                    <h2>제목: </h2>
-                                    <h2>0/0</h2>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className='chat_box'>
-                            <ul>
-                                <li>
-                                    <p>nickname: text.</p>
-                                </li>
-                                <li>
-                                    <p>nickname: text.</p>
-                                </li>
-                                <li>
-                                    <p>nickname: text.</p>
-                                </li>
-                            </ul>
-                            <form id='messageForm' name="messageForm">
-                                <div className="input-group clearfix">
-                                    <input className='test1'/>
-                                    <input type="text" style={{ display: 'none' }} />
+                            <div className="room_container">
+                                <div className="list">
+                                    <span>No. 1</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
                                 </div>
-                            </form>
+                                <div className="list">
+                                    <span>No. 2</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
+                                </div>
+                                <div className="list">
+                                    <span>No. 3</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
+                                </div>
+                                <div className="list">
+                                    <span>No. 4</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
+                                </div>
+                                <div className="list">
+                                    <span>No. 5</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
+                                </div>
+                                <div className="list">
+                                    <span>No. 6</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
+                                </div>
+                                <div className="list">
+                                    <span>No. 7</span>
+                                    <h2>TEST 1 </h2>
+                                    <span>0 / 6</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="chat-page">
+                            <div className="chat-container">
+                                <ul id="messageArea">
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+                                    <li>
+                                        <span>NickName : 야</span>
+                                    </li>
+
+                                </ul>
+                                <form id="messageForm" name="messageForm">
+                                    <div className="form-group">
+                                        <div className="input-group clearfix">
+                                            <textarea id="message" placeholder="채팅 입력..." autoComplete="off" className="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
