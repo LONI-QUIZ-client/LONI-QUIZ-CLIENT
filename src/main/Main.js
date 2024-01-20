e5246247ccb6cf1e93fdc627b1fd74e4212ac54d
oyg9731 <oyg9731@naver.com>
2024-01-21 04:21:25 +0900

    주말에도 메인은 만들어진다...

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,54 +6,43 @@ import Header from "./Header";
 const Main = () => {
     return (
         <div>
-            <Header/>
-            <div className='main-box'>
-                <div className='l-g'>
-                    <h1 className='logo'>LONI<br/>QUIZ</h1>
-                    <p>AI가 만든 이미지로 퀴즈 게임을 즐겨보세요</p>
-                    <Button/>
+            <div className='container'>
+                <Header/>
+                <div className='main-box'>
+                    <div className='l-g'>
+                        <h1 className='logo'>LONI<br/>QUIZ</h1>
+                        <p>AI가 만든 이미지로 퀴즈 게임을 즐겨보세요</p>
+                        <Button/>
+                    </div>
                 </div>
-                <div className='r-g'>
-                    <div className="t-img">
-                        <div className="box-wrap">
-                            <div className="t-box">
-                                <div className="img">
-                                    <img src={process.env.PUBLIC_URL + "/img/ASD.jpeg"} alt="Hover Effect"/>
-                                </div>
-                                <div className="info">
-                                    <h3>Design</h3>
-                                    <p>일러스트를 이용한 디자인입니다.</p>
-                                </div>
-                            </div>
-                            <div className="t-box">
-                                <div className="img">
-                                    <img src={process.env.PUBLIC_URL + "/img/Main.png"} alt="Hover Effect"/>
-                                </div>
-                                <div className="info">
-                                    <h3>Design</h3>
-                                    <p>일러스트를 이용한 디자인입니다.</p>
-                                </div>
-                            </div>
-                            <div className="t-box">
-                                <div className="img">
-                                    <img src={process.env.PUBLIC_URL + "/img/AS.png"} alt="Hover Effect"/>
-                                </div>
-                                <div className="info">
-                                    <h3>Design</h3>
-                                    <p>일러스트를 이용한 디자인입니다.</p>
-                                </div>
-                            </div>
+            </div>
+            <div className='content-box'>
+                <div className="box-wrap">
+                    <div className="t-box">
+                        <div className="img">
+                            <img src={process.env.PUBLIC_URL + "/img/ASD.jpeg"} alt="Hover Effect"/>
+                        </div>
+                        <div className="info">
+                            <h3>Design</h3>
+                            <p>일러스트를 이용한 디자인입니다.</p>
                         </div>
                     </div>
-                    <div className="box-wrap">
-                        <div className="b-box">
-                            <div className="img">
-                                <img src={process.env.PUBLIC_URL + "/img/gigi.jpg"} alt="Hover Effect"/>
-                            </div>
-                            <div class="info">
-                                <h3>Design</h3>
-                                <p>일러스트를 이용한 디자인입니다.</p>
-                            </div>
+                    <div className="t-box">
+                        <div className="img">
+                            <img src={process.env.PUBLIC_URL + "/img/Main.png"} alt="Hover Effect"/>
+                        </div>
+                        <div className="info">
+                            <h3>Design</h3>
+                            <p>일러스트를 이용한 디자인입니다.</p>
+                        </div>
+                    </div>
+                    <div className="t-box">
+                        <div className="img">
+                            <img src={process.env.PUBLIC_URL + "/img/AS.png"} alt="Hover Effect"/>
+                        </div>
+                        <div className="info">
+                            <h3>Design</h3>
+                            <p>일러스트를 이용한 디자인입니다.</p>
                         </div>
                     </div>
                 </div>
