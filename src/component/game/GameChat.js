03482a387da6cdbe36770f68490e2477dad0bdb1
conan <sarimiro@naver.com>
2024-01-24 17:39:49 +0900

    오늘치 커밋.

--- src/component/game/GameChat.js
+++ src/component/game/GameChat.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../scss/GameLobby.scss';
 import '../css/GameLobby.css';
 
 const GameInput = () => {
+    const [input,setInput]=useState('');
+    function inputSubmit(e) {
+        e.preventDefault();
+        setInput(document.getElementById('message').value);
+        console.log(input);
+
+    }
+
     return (
         <>
             <div className="chat-container">
                 <ul id="messageArea">
-                    <li>
+                    <li className="aaa">
                         <span>NickName : 야</span>
                     </li>
                 </ul>
-                <form id="messageForm" name="messageForm">
+                <form id="messageForm" name="messageForm" onSubmit={inputSubmit}>
                     <div className="form-group">
                         <div className="input-group clearfix">
-                            <textarea id="message" placeholder="채팅 입력..." autoComplete="off" className="form-control" ></textarea>
+                            <input id="message" placeholder="채팅 입력..." autoComplete="off" className="form-control"></input>
                         </div>
                     </div>
                 </form>
--- src/config/host-config.js
+++ src/config/host-config.js
@@ -1,6 +1,8 @@
 const LOCAL_PORT = '8888';
 const API_BASE_URL = 'http://localhost:' + LOCAL_PORT;
+const CHAT = '/game/lobbyChat'
 
-    const LOBBY = '/game/lobby';
+const LOBBY = '/game/lobby';
 
 export const LOBBY_URL = API_BASE_URL + LOBBY;
+export const LOBBY_CHAT = API_BASE_URL + CHAT;
