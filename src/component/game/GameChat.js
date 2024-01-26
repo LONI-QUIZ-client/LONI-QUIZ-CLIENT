172f572b7717e8a944de49c0084785798b23a855
conan <sarimiro@naver.com>
2024-01-26 17:16:13 +0900

    모달 생성 커밋
    - 채팅 스크롤 수정(역방향으로 시작)
    - 모달 구현 (500에러로 인해 POST가 작동이 안됨)

--- src/component/game/GameChat.js
+++ src/component/game/GameChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import '../scss/GameLobby.scss';
 import '../css/GameLobby.css';
 import { LOBBY_CHAT } from "../../config/host-config";
@@ -7,7 +7,7 @@ const GameChat = () => {
     const [input, setInput] = useState('');
     const [chatData, setChatData] = useState([]);
     const userId = "yy123";
-
+    const messageAreaRef = useRef();
 
     const inputSubmit = (e) => {
         e.preventDefault();
@@ -35,6 +35,9 @@ const GameChat = () => {
             .then(json => {
                 console.log(json);
                 setChatData(json.chats || []);
+
+                // 스크롤을 최하단으로 이동
+                scrollToBottom();
             })
             .catch(error => {
                 console.error("Error fetching chat data:", error);
@@ -42,12 +45,22 @@ const GameChat = () => {
             });
     }, []);
 
+    useEffect(() => {
+        scrollToBottom();
+    }, [chatData]);
+
+    const scrollToBottom = () => {
+        if (messageAreaRef.current) {
+            messageAreaRef.current.scrollTop = messageAreaRef.current.scrollHeight;
+        }
+    }
+
     return (
         <>
             <div className="chat-container">
-                <ul id="messageArea">
+                <ul id="messageArea" ref={messageAreaRef}>
                     {chatData.map((item, index) => (
-                        <li key={index} reversed>
+                        <li key={index}>
                             <span>{item.nickName}: {item.allCmContent}</span>
                         </li>
                     ))}
