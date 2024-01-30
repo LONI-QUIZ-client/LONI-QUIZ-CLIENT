690e6a548b219f438ec5a61e1035f38db62bdd3f
conan <sarimiro@naver.com>
2024-01-30 16:13:43 +0900

    게임 프로필 토큰 성공

--- src/component/game/GameProfile_Lobby.js
+++ src/component/game/GameProfile_Lobby.js
@@ -1,16 +1,24 @@
-import React from 'react';
+// GameProfileLobby 컴포넌트
+
+import React, {useState} from 'react';
 import catImage from "../../assets/img/star.png";
 import '../scss/GameLobby.scss';
 import '../css/GameLobby.css';
+import GameChat from './GameChat';
+import {getCurrentLoginUser} from "../../config/login-util";  // GameChat 컴포넌트 추가
 
 const GameProfileLobby = () => {
+    const currentUserNickname = getCurrentLoginUser()?.username || '';
+
+
+
     return (
         <div className='profile'>
             <div className='profile_image'>
                 <img src={catImage} alt="고양이 프로필"/>
             </div>
-            <p>NickName</p>
-            <p>0승 0패</p>
+            <p>{currentUserNickname}</p>
+            <p>total</p>
         </div>
     );
 };
