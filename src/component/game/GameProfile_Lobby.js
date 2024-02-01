64049831f52341de9b9b054cf37a8b9d72737340
conan <sarimiro@naver.com>
2024-02-01 17:01:33 +0900

    사진 출력 완료
    디자인 수정중.

--- src/component/game/GameProfile_Lobby.js
+++ src/component/game/GameProfile_Lobby.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import '../scss/GameLobby.scss';
 import '../scss/GameBbox1.scss';
 import '../css/GameLobby.css';
+import starImage from "../../assets/img/star.png";
 import GameChat from './GameChat';
 import { getCurrentLoginUser } from "../../config/login-util";
 import { JOIN_URL } from "../../config/host-config";
@@ -11,21 +12,39 @@ const GameProfileLobby = () => {
     const currentUserNickname = getCurrentLoginUser()?.username || '';
     const [imageFile, setImageFile] = useState(null);
 
+    const fetchProfileImage = async () => {
+
+        const url = JOIN_URL + "/profile-image";
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + getCurrentLoginUser().token
+            }
+        });
+
+        if (res.status === 200) {
+            const profileData = await res.blob();
+
+            // blob이미지를 url로 변환
+            const imageFile = window.URL.createObjectURL(profileData);
+
+            setImageFile(imageFile);
+        } else {
+            const errMsg = await res.text();
+            alert(errMsg);
+            setImageFile(null);
+        }
+
+    };
+
     useEffect(() => {
-        fetch(JOIN_URL + '/profile-image')
-            .then(res => res.blob())
-            .then(blob => {
-                setImageFile(URL.createObjectURL(blob));
-            })
-            .catch(error => {
-                console.error('프로필 이미지 가져오기 실패:', error);
-            });
-    }, []);
+        currentUserNickname && fetchProfileImage();
+    }, [currentUserNickname]);
 
     return (
         <div className='profile'>
             <div className='profile_image'>
-                {imageFile && <img src={imageFile} alt="유저 프로필" />}
+                <img src={imageFile || starImage} alt='프로필 사진'/>
             </div>
             <p>{currentUserNickname}</p>
             <p>total</p>
