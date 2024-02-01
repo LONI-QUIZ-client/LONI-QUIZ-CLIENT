86497f25c2a0b5a9a77433d1faf94a06e4a94b0f
SeungHanLee <swq3064@naver.com>
2024-02-01 17:21:56 +0900

    #살려주세요

--- src/component/game/GameInput.js
+++ src/component/game/GameInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import '../scss/GameLobby.scss';
 import '../css/GameLobby.css';
 import {redirect, useNavigate} from "react-router-dom";
@@ -6,9 +6,10 @@ import {ID, USERNAME} from '../../config/login-util';
 import SockJS from "sockjs-client";
 import {Stomp} from "@stomp/stompjs";
 
-const GameInput = ({ data }) => {
+const GameInput = ({data}) => {
     const itemsPerPage = 6; // 한 페이지당 보여질 아이템 개수
     const [currentPage, setCurrentPage] = useState(1);
+    const [fullMember, setFullMember] = useState(false);
     const userId = localStorage.getItem(ID);
     const username = localStorage.getItem(USERNAME);
 
@@ -33,33 +34,64 @@ const GameInput = ({ data }) => {
 
     const redirect = useNavigate()
 
+    useEffect(() => {
+        // Connect to WebSocket server
+        const socket = new SockJS('http://localhost:8888/ws');
+        const stompClient = Stomp.over(socket);
+        stompClient.connect({}, () => {
+            stompClient.subscribe('/topic/game/members', memberList => {
+                const receivedUsers = JSON.parse(memberList.body);
+                console.log(receivedUsers)
+                const isFullBoolean = !!receivedUsers.isFull;
+                console.log(isFullBoolean)
+                setFullMember(isFullBoolean)
+                // console.log(fullMember);
+
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        console.log(fullMember);
+        move(null);
+    }, [fullMember]);
+
     const StartGameRoom = (roomId, maxCount) => {
         const socket = new SockJS('http://localhost:8888/ws');
         const stompClient = Stomp.over(socket);
 
+        console.log("전송!!!!!!!!!")
         stompClient.connect({}, () => {
             stompClient.send("/app/game/members", {}, JSON.stringify({
-                userId : userId,
+                userId: userId,
                 gno: roomId,
                 username: username,
                 maxUser: maxCount
             }));
-
         });
+        // redirect('/gameRoom', {state: {roomId}});
+        // setTimeout(function() {
+        //     move(roomId)
+        // }, 3000);
 
-
-
-        redirect('/gameRoom', {state:{roomId}});
     }
-
-
+    const move = (roomId) => {
+        console.log(fullMember)
+        if (fullMember) {
+            alert("방 다찼다")
+        } else {
+            // redirect('/gameRoom', {state: {roomId}});
+        }
+    }
 
     return (
         <>
             <div className='room_list'>
                 {getCurrentPageItems().map((item, index) => (
-                    <div className="room_container" key={index} onClick={() => {StartGameRoom(item.gno, item.maxCount)}}>
-                        <div className="list" >
+                    <div className="room_container" key={index} onClick={() => {
+                        StartGameRoom(item.gno, item.maxCount)
+                    }}>
+                        <div className="list">
                             <p>No. {index + 1 + (currentPage - 1) * itemsPerPage}</p>
                             <h2>{item.title}</h2>
                             <p>{item.userCount} / {item.lobbyMaxCount}</p>
@@ -69,7 +101,7 @@ const GameInput = ({ data }) => {
                 ))}
             </div>
             <div className="pagination">
-                {Array.from({ length: totalPageCount }, (_, i) => i + 1).map((pageNumber) => (
+                {Array.from({length: totalPageCount}, (_, i) => i + 1).map((pageNumber) => (
                     <button
                         key={pageNumber}
                         onClick={() => handlePageChange(pageNumber)}
