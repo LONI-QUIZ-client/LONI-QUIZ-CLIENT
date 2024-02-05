9f759b9ffaa6e2e7916e77c00eff114d86aad1fb
conan <sarimiro@naver.com>
2024-02-05 13:38:49 +0900

    isAuthLogin 추가

--- src/config/login-util.js
+++ src/config/login-util.js
@@ -6,7 +6,9 @@ export const USERNAME = 'USER_NAME';
 export const USERID = 'USER_ID';
 
 // 로그인 여부를 확인하는 함수
-export const isLogin = () => !!localStorage.getItem(TOKEN);
+export const isLogin = () => !!sessionStorage.getItem(TOKEN);
+
+export const isAutoLogin = () => !!localStorage.getItem(TOKEN);
 
 // 로그인한 사용자의 데이터를 반환하는 함수
 export const getCurrentLoginUser = () => {
