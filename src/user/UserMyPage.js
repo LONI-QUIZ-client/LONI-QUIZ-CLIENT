2b8d8987dcb59050a9a3769c72f43efd7dd1bb26
YoungHeeSo <apdlfdldjqtm@gmail.com>
2024-02-06 17:36:21 +0900

    회원탈퇴 일단 만듬

--- src/user/UserMyPage.js
+++ src/user/UserMyPage.js
@@ -7,6 +7,7 @@ import {getLoginUserCheck, isAutoLogin, isLogin} from "../config/login-util";
 import cn from "classnames";
 import {LuPower, LuPowerOff} from "react-icons/lu";
 import "./scss/UserMyPage.scss"
+import MemberGetOut from "./MemberGetOut";
 const UserMyPage = () => {
 
     const redirect = useNavigate();
@@ -60,8 +61,6 @@ const UserMyPage = () => {
             }
         }
 
-
-
     }, [userPageInfo.id]);
 
     // 로비 이동
@@ -77,6 +76,27 @@ const UserMyPage = () => {
         , marginRight: '0.2rem'
     }
 
+    const [arrColor, setArrColor] = useState([]);
+    const colorArray = ['#FF7759', '#FFE77C', '#AAFF59', '#59FFA5', '#59F5FF', '#59C8FF', '#5975FF', '#9459FF','#FF5986'];
+    useEffect(() => {
+        const sendColor = [];
+        for (let i = 0; i < 2; i++) {
+            const num = Math.floor(Math.random() * colorArray.length);
+            sendColor.push(colorArray[num]);
+        }
+        setArrColor(sendColor);
+
+    }, []);
+
+    const backgroundHandler = {
+        background: arrColor.length === 2 ? `linear-gradient(135deg, ${arrColor[0]} 0%, ${arrColor[1]} 100%)` : ''
+    }
+
+    const iconHandler = {
+        background: arrColor.length === 2 ? `linear-gradient(135deg, ${arrColor[0]} 0%, ${arrColor[1]} 100%)` : ''
+        , boxShadow: `-5px -5px 10px 5px ${arrColor[0]+50}, 5px 5px 10px 5px ${arrColor[1]+50}`
+    }
+
     return (
         <div className={"user-info"}>
             <div className={"user-setting-buttons"}>
@@ -87,19 +107,17 @@ const UserMyPage = () => {
                     MOVE LOBBY
                 </button>
                 { userPageMaster ? <Logout /> : '' }
+                { userPageMaster ? <MemberGetOut /> : ''}
             </div>
 
-            <div className={"user-page-background"}>
+            <div className={"user-page-background"} style={backgroundHandler}>
                 <div className={"user-info-item-content"}>
-                    <div className={"user-page-profile"}>
-                        <BsFillPersonFill />
-                    </div>
+                    <div className={"user-page-profile"}><BsFillPersonFill /></div>
                     <div className={"user-info-contain"}>
-                        <div className={"user-info-change-item"}>
-                            {userPageInfo.nickname}
-                        </div>
+                        <div className={"user-name-item"}>{userPageInfo.nickname}</div>
                         <div className={"user-login-state-item"}>
-                            <div className={cn("logout-state-icon", {'login-state-icon':isUserLoginState})}>
+                            <div
+                                className={cn("logout-state-icon", {'login-state-icon':isUserLoginState})} style={iconHandler}>
                                 { isUserLoginState ? <LuPower /> : <LuPowerOff /> }
                             </div>
                             <div className={cn("logout-state-modal", {'login-state-modal':isUserLoginState})}>
@@ -110,10 +128,12 @@ const UserMyPage = () => {
                 </div>
             </div>
 
-            <div className={"user-game-info-contain"}>
-
+            <div className={"user-follow-info-contain"}>
+                <div className={"follow-item"}>
+                    follow
+                    count
+                </div>
             </div>
-
         </div>
     );
 };
