e5f2a92318ec07b4005122fe687d74c6b380ceb4
conan <sarimiro@naver.com>
2024-02-06 14:46:07 +0900

    친구 모달 뼈대만 제작
    세부디자인 작성중

--- src/component/game/GameFriends.js
+++ src/component/game/GameFriends.js
@@ -1,25 +1,89 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import '../scss/GameModal.scss';
-import {FormControlLabel, Switch, TextField} from "@mui/material";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
-import {Input} from "reactstrap";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
+import { InputBase, IconButton, Paper } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+
+const AddFriendSection = () => (
+    <div className='add_friend_menu'>
+        <h2>Add Friend</h2>
+        <div className=''>
+            <div className='lobby_search_friend_box'>
+                <Paper
+                    component="form"
+                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
+                >
+                    <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder="Search User"
+                        inputProps={{ 'aria-label': 'search user' }}
+                    />
+                    <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                        <SearchIcon />
+                    </IconButton>
+                </Paper>
+            </div>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    친구 1
+                </li>
+                <li>
+                    친구 2
+                </li>
+            </ul>
+        </div>
+    </div>
+);
+
+const FriendListSection = () => (
+    <div className='list_friend_menu'>
+        <h2>Friend List</h2>
+        <ul>
+            <li>Friend 1</li>
+            <li>Friend 2</li>
+            <li>Friend 3</li>
+        </ul>
+    </div>
+);
+
+const ChatSection = () => (
+    <div className='chat_friend_menu'>
+        <h2>Chat</h2>
+        <div>
+            {/* Display Chat Messages */}
+        </div>
+        <input type="text" placeholder="Type your message" />
+        <button>Send</button>
+    </div>
+);
 
 const GameFriends = () => {
     const ariaLabel = { 'aria-label': 'description' };
     const [open, setOpen] = useState(false);
+    const [activeSection, setActiveSection] = useState('addFriend');
+
     const handleOpen = () => {
         setOpen(true);
     };
 
     const handleClose = () => {
         setOpen(false);
+    };
+
+    const renderActiveSection = () => {
+        switch (activeSection) {
+            case 'addFriend':
+                return <AddFriendSection />;
+            case 'friendList':
+                return <FriendListSection />;
+            case 'chat':
+                return <ChatSection />;
+            default:
+                return null;
+        }
     };
 
     return (
@@ -32,44 +96,19 @@ const GameFriends = () => {
                 aria-labelledby="modal-title"
                 aria-describedby="modal-description"
             >
-                <Box sx={{ width: 300, bgcolor: 'background.paper', p: 3 ,borderRadius: 2, textAlign: 'center'}}>
+                <Box sx={{ width: 300, bgcolor: 'background.paper', p: 3, borderRadius: 2, textAlign: 'center' }}>
                     <div className=''>
-                        <div className='lobby_search_friend_box'>
-                            <Paper
-                                component="form"
-                                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
-                            >
-                                <InputBase
-                                    sx={{ ml: 1, flex: 1 }}
-                                    placeholder="Search User"
-                                    inputprops={{ 'aria-label': 'search user' }}
-                                />
-                                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                                    <SearchIcon />
-                                </IconButton>
-                            </Paper>
-                        </div>
                         <div className=''>
                             <ul>
-                                <li>
-                                    친구 1
-                                </li>
-                                <li>
-                                    친구 2
-                                </li>
-                                <li>
-                                    친구 3
-                                </li>
+                                <li onClick={() => setActiveSection('addFriend')}>Add Friend</li>
+                                <li onClick={() => setActiveSection('friendList')}>Friend List</li>
+                                <li onClick={() => setActiveSection('chat')}>Chat</li>
                             </ul>
                         </div>
                     </div>
                     <div className=''>
-                        <div>
-                            <p>친구 1</p>
-                            <button onClick={handleClose}>나가기</button>
-                        </div>
-                        <hr></hr>
-                        <div>채팅구역</div>
+                        {/* Render the active section based on the state */}
+                        {renderActiveSection()}
                     </div>
                 </Box>
             </Modal>
