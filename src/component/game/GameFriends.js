570c935ebb9c6e07ecbb49cee9360b5162ee892b
conan <sarimiro@naver.com>
2024-02-06 16:48:48 +0900

    친구 모달 SCSS 제작하기(위치 지정)
    className이 많은 관계로 일단 안전하게 커밋.
    add_friend_menu: 친구추가 메뉴 전체 다 감쌈
    lobby_search_friend_box: 검색창 감쌈
    search_friend_list: 검색하면 나오는 친구 리스트
    list_friend_menu: 친구 목록 리스트 감쌈
    chat_friend_menu: 1:1 채팅 감싸는 곳
    chating_texts: 채팅 오가는 내용 저장할 리스트
    lobby_friends: 모달 전체를 감쌈(버튼 포함)
    create_room_btn_friend: 모달 안으로 들어갈 버튼
    friends_box: 친구 모달 전체적 디자인
    game_friend_modal_container: 모달 내 보여지는 내용 감싸는 부분
    game_friend_modal_page: 메뉴 누르면 페이지 보여줌
    game_friend_modal_menu: 전체 메뉴 칸
    friends_menu_list 메뉴 묶는 칸
    add_fri_btn: 추가 메뉴
    list_fri_btn: 리스트 메뉴
    chat_fri_btn: 채팅 메뉴

--- src/component/game/GameFriends.js
+++ src/component/game/GameFriends.js
@@ -2,30 +2,33 @@ import React, { useState } from 'react';
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import '../scss/GameModal.scss';
+import '../scss/GameFriends.scss';
+import addFriendBtn from "../../assets/img/baseline_person_add_black_24dp.png";
+import listFriendBtn from "../../assets/img/baseline_view_list_black_24dp.png";
+import chatFriendBtn from "../../assets/img/baseline_question_answer_black_24dp.png";
 import { InputBase, IconButton, Paper } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+
 
 const AddFriendSection = () => (
     <div className='add_friend_menu'>
         <h2>Add Friend</h2>
-        <div className=''>
-            <div className='lobby_search_friend_box'>
-                <Paper
-                    component="form"
-                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
-                >
-                    <InputBase
-                        sx={{ ml: 1, flex: 1 }}
-                        placeholder="Search User"
-                        inputProps={{ 'aria-label': 'search user' }}
-                    />
-                    <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                </Paper>
-            </div>
+        <div className='lobby_search_friend_box'>
+            <Paper
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
+            >
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search User"
+                    inputProps={{ 'aria-label': 'search user' }}
+                />
+                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
         </div>
-        <div>
+        <div className='search_friend_list'>
             <ul>
                 <li>
                     친구 1
@@ -41,27 +44,30 @@ const AddFriendSection = () => (
 const FriendListSection = () => (
     <div className='list_friend_menu'>
         <h2>Friend List</h2>
-        <ul>
-            <li>Friend 1</li>
-            <li>Friend 2</li>
-            <li>Friend 3</li>
-        </ul>
+        <div>
+            <ul>
+                <li>Friend 1</li>
+                <li>Friend 2</li>
+                <li>Friend 3</li>
+            </ul>
+        </div>
     </div>
 );
 
 const ChatSection = () => (
     <div className='chat_friend_menu'>
         <h2>Chat</h2>
-        <div>
+        <div className='chating_texts'>
             {/* Display Chat Messages */}
         </div>
-        <input type="text" placeholder="Type your message" />
-        <button>Send</button>
+        <div>
+            <input type="text" placeholder="Type your message" />
+            <button>Send</button>
+        </div>
     </div>
 );
 
 const GameFriends = () => {
-    const ariaLabel = { 'aria-label': 'description' };
     const [open, setOpen] = useState(false);
     const [activeSection, setActiveSection] = useState('addFriend');
 
@@ -97,18 +103,29 @@ const GameFriends = () => {
                 aria-describedby="modal-description"
             >
                 <Box sx={{ width: 300, bgcolor: 'background.paper', p: 3, borderRadius: 2, textAlign: 'center' }}>
-                    <div className=''>
-                        <div className=''>
-                            <ul>
-                                <li onClick={() => setActiveSection('addFriend')}>Add Friend</li>
-                                <li onClick={() => setActiveSection('friendList')}>Friend List</li>
-                                <li onClick={() => setActiveSection('chat')}>Chat</li>
+                    <div className='game_friend_modal_container'>
+                        <div className='game_friend_modal_page'>
+                            {renderActiveSection()}
+                        </div>
+                        <div className='game_friend_modal_menu'>
+                            <ul className='friends_menu_list'>
+                                <li>
+                                    <button className='add_fri_btn' onClick={() => setActiveSection('addFriend')}>
+                                        <img src={addFriendBtn} alt='친구 추가'/>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button className='list_fri_btn' onClick={() => setActiveSection('friendList')}>
+                                        <img src={listFriendBtn} alt='친구 목록'/>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button className='chat_fri_btn' onClick={() => setActiveSection('chat')}>
+                                        <img src={chatFriendBtn} alt='친구 1:1 채팅'/>
+                                    </button>
+                                </li>
                             </ul>
                         </div>
-                    </div>
-                    <div className=''>
-                        {/* Render the active section based on the state */}
-                        {renderActiveSection()}
                     </div>
                 </Box>
             </Modal>
