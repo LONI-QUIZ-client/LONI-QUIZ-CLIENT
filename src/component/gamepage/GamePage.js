dfce308658ad2666ae9ff4e4fc0e89333e3c9cd5
oyg9731 <oyg9731@naver.com>
2024-02-12 22:58:34 +0900

    모달 뿌시기

--- src/component/gamepage/GamePage.js
+++ src/component/gamepage/GamePage.js
@@ -547,22 +547,24 @@ const GamePage = () => {
                                         }
                                     }}
                                 />
-                                <button className='create' onClick={() => {
-                                    createImage();
-                                    setShowSpinner(true); // 사진 만들기 버튼이 클릭되었을 때 스피너를 표시합니다.
-                                }}>
-                                    사진만들기
-                                </button>
-                                <button
-                                    className={'modal-close-btn'}
-                                    onClick={() => {
-                                        sendImageHandler();
-                                        timeHandler();
-                                    }}
-                                    disabled={selectedImage === null} // selectedImage가 null이 아닌 경우에만 활성화됩니다.
-                                >
-                                    모달 닫기 및 이미지 전송
-                                </button>
+                                <div className='buttons'>
+                                    <button className='create' onClick={() => {
+                                        createImage();
+                                        setShowSpinner(true); // 사진 만들기 버튼이 클릭되었을 때 스피너를 표시합니다.
+                                    }}>
+                                        사진만들기
+                                    </button>
+                                    <button
+                                        className={'modal-close-btn'}
+                                        onClick={() => {
+                                            sendImageHandler();
+                                            timeHandler();
+                                        }}
+                                        disabled={selectedImage === null} // selectedImage가 null이 아닌 경우에만 활성화
+                                    >
+                                        모달 닫기 및 이미지 전송
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
