a4a199241d5b1c055bf1b09af9a235d6aa3db944
conan <sarimiro@naver.com>
2024-02-14 17:21:50 +0900

    Header 프로필 사진 띄우기 성공
    [남은 할 일]
    1. 프로필 하단 total 띄우기
    2. 팔로우, 채팅 기능 구현
    3. 전체 디자인 수정
    4. 헤더 로그인, 로그아웃 일부 수정 필요.(로그아웃이 감지가 안되는 상태.)

--- src/component/main/Header.js
+++ src/component/main/Header.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './scss/Header.scss';
-import {isLogin} from "../../config/login-util";
+import {getAutoCurrentLoginUser, getCurrentLoginUser, isLogin} from "../../config/login-util";
 import {useNavigate} from "react-router-dom";
 import starImage from "../../assets/img/star.png";
+import logoImage from "../../user/scss/img/project-logo.png";
+import {PROFILE_URL} from "../../config/host-config";
 
 const Header = () => {
+    const [HimageFile, setHImageFile] = useState(null);
+    const currentUser = (isLogin() ? getCurrentLoginUser() : getAutoCurrentLoginUser());
+    const currentUserNickname = currentUser?.username || '';
+    const userId = currentUser?.id || '';
     const nav = useNavigate();
     const logoutHandler = e => {
         localStorage.clear();
@@ -12,9 +18,31 @@ const Header = () => {
         // setImgUrl(null);
         // redirection('/login');
     };
+
+    const fetchProfileImageHeader = async () => {
+        const res = await fetch(PROFILE_URL, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + currentUser.token
+            }
+        });
+
+        if (res.status === 200) {
+            const profileData = await res.blob();
+            const HimageFile = window.URL.createObjectURL(profileData);
+            setHImageFile(HimageFile);
+        } else {
+            const errMsg = await res.text();
+            alert(errMsg);
+            setHImageFile(null);
+        }
+    };
+    useEffect(() => {
+        currentUserNickname && fetchProfileImageHeader();
+    }, [currentUserNickname]);
     return (
         <div className="h-box">
-            <img src={process.env.PUBLIC_URL + "/img/LOLO.png"} alt="asd" className='header_logo'/>
+            <img src={logoImage} alt="asd" className='header_logo'/>
             <div className="header">
                 <table>
                     <tbody>
@@ -22,7 +50,7 @@ const Header = () => {
                             <td className="header-table">
                                 <li>
                                     <div>
-                                        <img src={starImage} alt='미니 프로필 사진'  className='header_profile_image'/>
+                                        <img src={HimageFile || starImage} alt='미니 프로필 사진'  className='header_profile_image'/>
                                     </div>
                                 </li>
                                 <li>
